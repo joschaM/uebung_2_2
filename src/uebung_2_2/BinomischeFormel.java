f96f662f75fc383c47ed16ada7964a8f4b2da4f3
14508 <14508@A101-20.nordakademie.intern>
2016-10-21 11:40:05 +0200

    Binomische Formel

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,12 @@
+package uebung_2_2;
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		
+		double a = 8;
+		double b = 3;
+		
+		System.out.println("(a+b)*(a+b) = " + (a*a + 2*a*b + b*b));
+	}
+}
